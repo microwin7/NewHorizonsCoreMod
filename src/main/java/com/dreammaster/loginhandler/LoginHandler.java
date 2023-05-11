63c70d46c63f205d677f0c70287c20b2a2bc6885
microwin7 <usa.microwin8@gmail.com>
2023-05-11 12:57:12 +0300

    remove login message

--- src/main/java/com/dreammaster/loginhandler/LoginHandler.java
+++ src/main/java/com/dreammaster/loginhandler/LoginHandler.java
@@ -1,34 +1,13 @@
 package com.dreammaster.loginhandler;
 
-import static com.dreammaster.config.CoreModConfig.ModPackVersion;
 import static net.minecraft.util.EnumChatFormatting.*;
-
-import java.util.Arrays;
-
-import net.minecraft.util.ChatComponentText;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent;
 
 public class LoginHandler {
 
-    private static final String[] LOGIN_TEXT = {
-            GOLD.toString() + STRIKETHROUGH + "-----------------------------------------------------",
-            BOLD + "Welcome to Gregtech: New Horizons " + GREEN + ModPackVersion,
-            RED + "WARNING: ExtraCells 2 is now officially deprecated",
-            RED + "and will be removed in a future version.",
-            RED + "All parts can be converted to their AE2FC equivalent", RED + "by putting them into a crafting grid.",
-            RED + "Better Questing Mod use now UUID for quest ID", RED + "and split up giant quest JSON file.",
-            RED + "This change is not revers-able.", RED + "Make a backup before save your Quest Book file",
-            BLUE + "The Quest Book has a shortcut key, check your keybindings", BLUE + "to remove conflicts.",
-            DARK_GREEN + "GTNH WIKI link https://gtnh.miraheze.org/wiki/", GREEN + "Please report bugs here:",
-            GOLD + "https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/",
-            BLUE + "Visit our discord at https://discord.gg/gtnh",
-            GOLD.toString() + STRIKETHROUGH + "-----------------------------------------------------" };
-
     @SuppressWarnings("unused")
     @SubscribeEvent
-    public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        Arrays.stream(LOGIN_TEXT).map(ChatComponentText::new).forEach(event.player::addChatMessage);
-    }
+    public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {}
 }
