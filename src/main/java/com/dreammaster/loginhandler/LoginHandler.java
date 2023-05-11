467bc0ffa6ddc123ba0b774b15761517e7866981
microwin7 <usa.microwin8@gmail.com>
2023-05-11 12:51:40 +0300

    Update LoginHandler.java

--- src/main/java/com/dreammaster/loginhandler/LoginHandler.java
+++ src/main/java/com/dreammaster/loginhandler/LoginHandler.java
@@ -12,18 +12,8 @@ import cpw.mods.fml.common.gameevent.PlayerEvent;
 
 public class LoginHandler {
 
-    private static final String[] LOGIN_TEXT = {
-            GOLD.toString() + STRIKETHROUGH + "-----------------------------------------------------",
-            BOLD + "Welcome to Gregtech: New Horizons " + GREEN + ModPackVersion,
-            BLUE + "The Quest Book has a shortcut key, check your keybindings", BLUE + "to remove conflicts.",
-            DARK_GREEN + "GTNH WIKI link https://gtnh.miraheze.org/wiki/", GREEN + "Please report bugs here:",
-            GOLD + "https://github.com/GTNewHorizons/GT-New-Horizons-Modpack/",
-            BLUE + "Visit our discord at https://discord.gg/gtnh",
-            GOLD.toString() + STRIKETHROUGH + "-----------------------------------------------------" };
-
     @SuppressWarnings("unused")
     @SubscribeEvent
     public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        Arrays.stream(LOGIN_TEXT).map(ChatComponentText::new).forEach(event.player::addChatMessage);
     }
 }
